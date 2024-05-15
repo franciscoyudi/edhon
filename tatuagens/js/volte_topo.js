45574af3087f7a025aead5480c4223c49eede111
Francisco Yudi <157151807+franciscoyudi@users.noreply.github.com>
2024-05-15 18:54:23 -0300

    Add files via upload

---
+++ tatuagens/js/volte_topo.js
@@ -0,0 +1,21 @@
+// Obtém o botão
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+// Função para rolar a página até o topo
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Adiciona o evento de clique ao botão
+backToTopBtn.addEventListener('click', scrollToTop);
+
+// Monitora o scroll da página
+window.onscroll = function() {
+  const section3 = document.getElementById("section3");
+  const rect = section3.getBoundingClientRect();
+  if (rect.top <= 0) { // Se a segunda seção está no topo ou acima do topo da janela
+    backToTopBtn.style.display = "block"; // Mostra o botão
+  } else {
+    backToTopBtn.style.display = "none"; // Esconde o botão
+  }
+};
